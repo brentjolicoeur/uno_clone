5e28bc1fdc3592e8574ccd043bd81a57ccbf8e44
brentjolicoeur <brentjolicoeur@gmail.com>
2025-07-21 14:59:35 -0400

    refactoring.  created Round class to track game state.

--- card.py
+++ card.py
@@ -1,4 +1,10 @@
 from termcolor import colored
+import random
+
+COLORS = ("red", "yellow", "green", "blue")
+STANDARD = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+SPECIAL = ("Skip", "Reverse", "Draw Two")
+WILDS = ("Wild", "Wild Draw Four")
 
 class Card:
     def __init__(self, color, value, special=False):
@@ -28,3 +34,23 @@ class Card:
                     colors_index += 1
                     colors_index %= 4
             return string
+
+def build_new_game_deck():
+    deck = []
+
+    for color in COLORS:
+        for value in STANDARD:
+            for _ in range(2):
+                deck.append(Card(color, value))
+        for value in SPECIAL:
+            for _ in range(2):
+                deck.append(Card(color, value, special=True))
+
+    for value in WILDS:
+        for _ in range(4):
+            deck.append(Card("Wild", value, special=True))
+
+    for _ in range(7):
+        random.shuffle(deck)
+
+    return deck
--- main.py
+++ main.py
@@ -5,30 +5,7 @@ from player import Player
 def main():
     players, draw_pile, discard_pile = game.start() #initialize game, create players, draw pile, deal hands, first discard
 
-    active_discard = discard_pile[-1]
-    active_color = active_discard.color
-    active_direction = 1  # will either be 1 or -1 to determine who next player is as we cycle through indices
-    next_player_index = 0
-
-    print(f"The top card of the Discard Pile is {active_discard}.")
-
-    # Handle special cards as the first discard
-    if active_discard.special:
-        match active_discard.value:
-            case "Skip":
-                print(f"Uh Oh! {players[next_player_index]} loses their first turn!")
-                next_player_index += 1
-            case "Reverse":
-                print("Looks like we are changing directions!")
-                active_direction *= -1
-            case "Draw Two":
-                print(f"Oh No! {players[next_player_index]} must draw 2 cards and loses their first turn.")
-                game.draw_card(players[next_player_index], draw_pile, 2)
-                next_player_index += 1
-            case _:
-                print(f"Looks like {players[next_player_index]} will get to pick the first color!")
-
-    print(f"The first to play will be {players[next_player_index]}")
+    first_round = game.Round(players, draw_pile, discard_pile)
     
     
     print(f"There are {len(draw_pile)} cards left in the Draw Pile.")
