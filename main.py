7e610c9592e84a6dbe96968a5a1f733d82d8bb3f
brentjolicoeur <brentjolicoeur@gmail.com>
2025-07-21 15:27:48 -0400

    refactor get valid plays

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import game
-from card import Card
-from player import Player
+#from card import Card
+#from player import Player
 
 def main():
     players, draw_pile, discard_pile = game.start() #initialize game, create players, draw pile, deal hands, first discard
 
     first_round = game.Round(players, draw_pile, discard_pile)
+
+    while not first_round.round_finished:
+        break
     
+    for player in first_round.players:
+        print(f"{player.name}'s hand is {player.hand}")
+        print(f"{player.name}'s valid plays are {player.get_valid_plays(first_round.active_discard)}")
     
     print(f"There are {len(draw_pile)} cards left in the Draw Pile.")
 
--- player.py
+++ player.py
@@ -9,31 +9,30 @@ class Player:
     def __repr__(self):
         return self.name
     
-    def get_valid_plays(self, active_color, current_discard):
+    def get_valid_plays(self, current_discard):
         valid_plays = []
-        color_matches = []  # will be used to check if Wild Draw Four is legal
 
-        if active_color == "Wild": #should *eventually* only ever happen if the first discard of the game is a Wild
+        if current_discard.color == "Wild": #should only ever happen if the first discard of the game is a Wild
             #all cards in hand are valid
             return self.hand
 
         for card in self.hand:
-            # all matching colors are legal
-            if card.color == active_color:
-                color_matches.append(card) # will use this set to extend valid_plays after checking for Draw Four
-            # add any value-matching, non-color-matching cards to valid_plays
-            if card.value == current_discard.value and card not in color_matches:
+            # all matching colors are valid
+            if card.color == current_discard.color:
                 valid_plays.append(card)
-        valid_plays.extend(color_matches)
-        #check if hand contains a Draw Four Wild and add to valid plays if it meets the criteria (no matching colors)
-        for card in self.hand:
-            if card.color == "Wild":
-                if card.value == "Wild Draw Four":
-                    if len(color_matches) == 0:
-                        #legal to play
-                        valid_plays.append(card)
-                    # Otherwise not legal to play--do nothing
-                else: # will only trigger if it's a regular wild
+            # check if Wild Draw Four is a valid play
+            elif card.value == "Wild Draw Four":
+                if self.valid_draw_four(current_discard.color):
                     valid_plays.append(card)
+            # add any value-matching, non-color-matching cards to valid_plays
+            elif card.value == current_discard.value or card.value == "Wild":
+                valid_plays.append(card)
 
         return valid_plays
+
+    def valid_draw_four(self, color):
+        valid = True
+        for card in self.hand:
+            if card.color == color:
+                valid = False
+        return valid
